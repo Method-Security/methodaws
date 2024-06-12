66aac3ea02d46546852c6505cb442efad054170a
Sean Hacker <sean@method.security>
2024-06-12 13:42:05 -0400

    Move dockerfile to top level

--- methodaws-docker/scripts/awss3enumerate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run methodaws s3 enumerate
-methodaws s3 enumerate --output signal --output-file /mnt/output/output.json "$@"
--- methodaws-docker/scripts/awss3ls.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run methodaws s3 ls
-methodaws s3 ls --output signal --output-file /mnt/output/output.json "$@"
