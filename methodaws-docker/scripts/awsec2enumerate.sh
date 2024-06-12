f318ac396f488ba9c4a53e826077ce3b9b4d1878
Sean Hacker <sean@method.security>
2024-06-12 13:42:05 -0400

    Move dockerfile to top level

--- methodaws-docker/scripts/awsec2enumerate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run methodaws ec2
-methodaws ec2 enumerate --output signal --output-file /mnt/output/output.json "$@"
--- methodaws-docker/scripts/awsrdsenumerate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run methodaws rds enumerate
-methodaws rds enumerate --output signal --output-file /mnt/output/output.json "$@"
