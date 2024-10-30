e25392534084cf0b3b56d3bb24a070ce1a8c8c4e
Qinfeng Chen <qinfeng@method.security>
2024-10-30 14:50:06 -0400

    Add v0 implementation of apigateway enumber (#46)
    * add v0 implementation of apigateway enumerate
    
    * add integration type
    
    * APIs
    
    * lint errors
    
    * remove unused types
    
    * commnets

---
+++ cmd/apigateway.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"github.com/Method-Security/methodaws/internal/apigateway"
+	"github.com/spf13/cobra"
+)
+
+func (a *MethodAws) InitAPIGatewayCommand() {
+	apiGatewayCmd := &cobra.Command{
+		Use:     "apigateway",
+		Short:   "Audit and manage api gateways",
+		Long:    `Audit and manage api gateways`,
+		Aliases: []string{"agw"},
+	}
+
+	enumerate := &cobra.Command{
+		Use:   "enumerate",
+		Short: "Enumerate api gateways",
+		Long:  `Enumerate api gateways in your AWS account.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			report, err := apigateway.EnumerateAPIGateway(cmd.Context(), *a.AwsConfig, a.RootFlags.Regions)
+			if err != nil {
+				a.OutputSignal.AddError(err)
+			}
+			a.OutputSignal.Content = report
+		},
+	}
+
+	apiGatewayCmd.AddCommand(enumerate)
+	a.RootCmd.AddCommand(apiGatewayCmd)
+}
--- go.mod
+++ go.mod
@@ -67,6 +67,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.21 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/apigateway v1.27.2
 	github.com/aws/aws-sdk-go-v2/service/autoscaling v1.40.5
 	github.com/aws/aws-sdk-go-v2/service/eks v1.42.1
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.2 // indirect
