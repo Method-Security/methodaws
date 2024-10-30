ec17f67c7b39aa4aacfb35aefad61f1e3a4b9d58
Qinfeng Chen <qinfeng@method.security>
2024-10-30 12:32:03 -0400

    commnets

--- cmd/apigateway.go
+++ cmd/apigateway.go
@@ -19,10 +19,8 @@ func (a *MethodAws) InitAPIGatewayCommand() {
 		Long:  `Enumerate api gateways in your AWS account.`,
 		Run: func(cmd *cobra.Command, args []string) {
 			report, err := apigateway.EnumerateAPIGateway(cmd.Context(), *a.AwsConfig, a.RootFlags.Regions)
-
 			if err != nil {
-				errorMessage := err.Error()
-				a.OutputSignal.ErrorMessage = &errorMessage
+				a.OutputSignal.AddError(err)
 				a.OutputSignal.Status = 1
 			}
 			a.OutputSignal.Content = report
