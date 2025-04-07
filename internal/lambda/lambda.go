faeea0e140247f43d35d950aebcc2548da38574d
Dan Paquette <dpaquette@method.security>
2025-04-07 16:40:01 -0400

    use fern enum parsers

--- internal/lambda/lambda.go
+++ internal/lambda/lambda.go
@@ -2,7 +2,6 @@ package lambda
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	methodaws "github.com/Method-Security/methodaws/generated/go"
@@ -10,36 +9,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/lambda"
 )
-
-func parseLambdaArchitecture(architecture string) (methodaws.LambdaArchitecture, error) {
-	switch architecture {
-	case "x86_64":
-		return methodaws.LambdaArchitectureX8664, nil
-	case "arm64":
-		return methodaws.LambdaArchitectureArm64, nil
-	}
-	return "", fmt.Errorf("%s is not a valid %T", architecture, methodaws.LambdaArchitecture(""))
-}
-
-func parseLambdaPackageType(packageType string) (methodaws.LambdaPackageType, error) {
-	switch packageType {
-	case "Zip":
-		return methodaws.LambdaPackageTypeZip, nil
-	case "Image":
-		return methodaws.LambdaPackageTypeImage, nil
-	}
-	return "", fmt.Errorf("%s is not a valid %T", packageType, methodaws.LambdaPackageType(""))
-}
-
-func parseLambdaLoggingFormat(logFormat string) (methodaws.LambdaLoggingFormat, error) {
-	switch logFormat {
-	case "Text":
-		return methodaws.LambdaLoggingFormatText, nil
-	case "JSON":
-		return methodaws.LambdaLoggingFormatJson, nil
-	}
-	return "", fmt.Errorf("%s is not a valid %T", logFormat, methodaws.LambdaLoggingFormat(""))
-}
 
 func EnumerateLambdaForRegion(ctx context.Context, cfg aws.Config, region string) ([]*methodaws.LambdaFunction, error) {
 	cfg.Region = region
@@ -62,14 +31,14 @@ func EnumerateLambdaForRegion(ctx context.Context, cfg aws.Config, region string
 
 			lambdaArchitectures := []methodaws.LambdaArchitecture{}
 			for _, architecture := range function.Architectures {
-				architecture, err := parseLambdaArchitecture(string(architecture))
+				architecture, err := methodaws.NewLambdaArchitectureFromString(string(architecture))
 				if err != nil {
 					return []*methodaws.LambdaFunction{}, err
 				}
 				lambdaArchitectures = append(lambdaArchitectures, architecture)
 			}
 
-			lambdaPackageType, err := parseLambdaPackageType(string(function.PackageType))
+			lambdaPackageType, err := methodaws.NewLambdaPackageTypeFromString(string(function.PackageType))
 			if err != nil {
 				return []*methodaws.LambdaFunction{}, err
 			}
@@ -85,7 +54,7 @@ func EnumerateLambdaForRegion(ctx context.Context, cfg aws.Config, region string
 
 			var loggingConfig *methodaws.LambdaLoggingConfig
 			if function.LoggingConfig != nil {
-				logFormat, err := parseLambdaLoggingFormat(string(function.LoggingConfig.LogFormat))
+				logFormat, err := methodaws.NewLambdaLoggingFormatFromString(string(function.LoggingConfig.LogFormat))
 				if err != nil {
 					return []*methodaws.LambdaFunction{}, err
 				}
